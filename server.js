607dbecc2bf56e9146d210342ee8295aca8ca8f8
Shohei Aoki <shoaok@gmail.com>
2015-05-10 20:00:35 +0900

    change to broadcast

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: port});
 
 wss.on('connection', function connection(ws) {
-    ws.on('open', function open() {
-	console.log('connected');
-    });
+  ws.on('open', function open() {
+    console.log('connected');
+  });
 
-    ws.on('close', function close() {
-	console.log('disconnected');
-    });
+  wss.broadcast = function(data) {
+    for (var i in this.clients)
+      this.clients[i].send(data);
+  };
 
-    ws.on('message', function message(data, flags) {
-	ws.send(data);
-    });
+  ws.on('close', function close() {
+    console.log('disconnected');
+  });
+
+  ws.on('message', function message(data, flags) {
+    wss.broadcast(message);
+    // ws.send(data);
+  });
 
 });
