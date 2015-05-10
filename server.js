e44b7c55868248c5cdfe304b98783b980db14733
Shohei Aoki <shoaok@gmail.com>
2015-05-10 20:07:03 +0900

    fix

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ console.log('listening on '+port);
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: port});
 
+wss.broadcast = function(data) {
+  for (var i in this.clients)
+    this.clients[i].send(data);
+};
+
 wss.on('connection', function connection(ws) {
-  ws.on('open', function open() {
+  ws.on('open', function () {
     console.log('connected');
   });
 
-  wss.broadcast = function(data) {
-    for (var i in this.clients)
-      this.clients[i].send(data);
-  };
 
-  ws.on('close', function close() {
+  ws.on('close', function () {
     console.log('disconnected');
   });
 
-  ws.on('message', function message(data, flags) {
-    wss.broadcast(message);
+  ws.on('message', function (data, flags) {
+    wss.broadcast(data);
     // ws.send(data);
   });
 
